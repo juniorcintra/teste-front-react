ecbb9be97f73167bbbf9ca9e0e318e8af37d3611
juniorcintra <junior.cintra.developer@gmail.com>
2024-10-10 07:53:19 -0300

    header Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,21 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUserStore } from "@/store/slices";
+import AvatarIMG from "@/assets/user.png";
+
 export default function Home() {
-  return <div>index</div>;
+  const { user } = useUserStore();
+  return (
+    <main className="flex h-screen w-full flex-col">
+      <div className="flex w-full flex-row items-center justify-between px-4 py-2">
+        <span>Lista de Contatos</span>
+        <div className="flex flex-row items-center gap-2 rounded-2xl border border-zinc-600 p-1">
+          <Avatar>
+            <AvatarImage src={AvatarIMG} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <span>{user?.name} </span>
+        </div>
+      </div>
+    </main>
+  );
 }
